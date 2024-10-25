17de2ae8f5b032764539efff0cc3b6bf3cd8d1aa
Raad Altarawneh <raedtarawneh555@gmail.com>
2024-10-26 00:55:57 +0200

    Update - Code

---
+++ src/components/Timestamp.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    timeStamp: Object
+})
+</script>
+
+<template>
+
+<span class="timestamp">{{tweets[0].timestamp}}</span>
+
+</template> 
+
+<style></style>
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,41 +1,45 @@
+<script setup>
+import Profileimage from './Profileimage.vue';
+import User from  './user.vue';
+import Timestamp from  './Timestamp.vue';
+import Message from './Message.vue'
+import Actions from './Action.vue';
+
+defineProps({
+  tweets: Object
+
+});
+
+
+</script>
+
 <template>
-  <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
 
-    <div class="body">
-      <div class="top">
-        <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
-        </span>
 
-        <span class="timestamp">Nov 30, 2020</span>
+<div class="tweet"></div>
+<Profileimage :image="user.image"></Profileimage>
+
+<div class="body" >
+
+  <div class="top">
+        <User :userData="Userdata"></User>
+        <Timestamp :time="timeStamp"></Timestamp>
       </div>
 
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
+      <Message :message="message"></Message>
 
-      <div class="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
-    </div>
+<Action></Action>
+</div>
 
-    <i class="fas fa-ellipsis-h"></i>
-  </div>
+<i class="fas fa-ellipsis-h"></i>
+</div>
+
 </template>
 
 <style scoped>
 a {
-  color: #42b983;
+color: #42b983;
 }
 </style>
+
+
